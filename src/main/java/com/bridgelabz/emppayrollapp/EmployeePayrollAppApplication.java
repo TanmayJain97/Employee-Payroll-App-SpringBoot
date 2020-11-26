96cc03ba716704bcd8f49cc0b11fde3b44055ea6
TanmayJain97 <mail.tanmay.jain@gmail.com>
2020-11-26 08:27:39 +0530

    Add: Runner Main class
    Updated to Day 49 Practice Problem

--- src/main/java/com/bridgelabz/emppayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/emppayrollapp/EmployeePayrollAppApplication.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.emppayrollapp;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
