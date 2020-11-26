52296d8a0166cff4fc6b31f378b8303ed15f4478
TanmayJain97 <mail.tanmay.jain@gmail.com>
2020-11-26 08:24:49 +0530

    Add: Employee Repository class

---
+++ src/main/java/com/bridgelabz/emppayrollapp/repo/EmpRepository.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.emppayrollapp.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bridgelabz.emppayrollapp.model.Employee;
+
+public interface EmpRepository extends JpaRepository<Employee, Long> {}
