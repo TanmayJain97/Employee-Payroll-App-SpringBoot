dfc17c0148d27f42975bdccd81b57d789d04ca34
TanmayJain97 <mail.tanmay.jain@gmail.com>
2020-11-27 08:08:35 +0530

    Add: Method to find by name

--- src/main/java/com/bridgelabz/emppayrollapp/repo/EmpRepository.java
+++ src/main/java/com/bridgelabz/emppayrollapp/repo/EmpRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bridgelabz.emppayrollapp.model.Employee;
 
-public interface EmpRepository extends JpaRepository<Employee, Long> {}
+public interface EmpRepository extends JpaRepository<Employee, Long> {
+	public Employee findByName(String name);
+}
